0269f626d1acc64ae2185e40b79eac49c8d029ac
AShenoy16 <ashe194@aucklanduni.ac.nz>
2023-03-17 22:30:32 +1300

    Made icons in header go to correct places

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,12 +4,17 @@ import { motion } from "framer-motion";
 import { logo } from "@/assets";
 import { styles } from "@/styles/styles";
 import Link from "next/link";
+import dynamic from "next/dynamic";
+
+const DynamicLink = dynamic(() => import("next/link"), {
+  ssr: false,
+});
 
 type Props = {};
 
 export default function Header({}: Props) {
   return (
-    <nav
+    <header
       className={`${styles.paddingX} w-full flex items-center py-2 fixed top-0 z-20 bg-black`}
     >
       {/* Above makes it stick to the top and automatically splits the icons in between */}
@@ -31,30 +36,42 @@ export default function Header({}: Props) {
         }}
         className=" w-full flex justify-between items-center max-w-5xl mx-auto"
       >
-        <img src={logo.src} alt="logo" className="w-11 h-11 object-contain mx-[-1px] md:mx-[-5px]" />
+        <div>
+          <Link href="#hero">
+            <img
+              src={logo.src}
+              alt="logo"
+              className="w-11 h-11 object-contain mx-[-1px] md:mx-[-5px] cursor-pointer hover:scale-150"
+            />
+          </Link>
+        </div>
 
         <div className="flex justify-start">
           <SocialIcon
+            className="hover:scale-110"
             url="https://github.com/AShenoy16"
             fgColor="gray"
             bgColor=" transparent"
           />
 
           <SocialIcon
+            className="hover:scale-110"
             url="https://www.linkedin.com/in/aditya-shenoy-692b5a247/"
             fgColor="gray"
             bgColor=" transparent"
           />
 
-          <SocialIcon
-            className="cursor-pointer"
-            network="email"
-            fgColor="gray"
-            bgColor=" transparent"
-          />
+          <DynamicLink href="#contact">
+            <SocialIcon
+              className="cursor-pointer hover:scale-110"
+              network="email"
+              fgColor="gray"
+              bgColor=" transparent"
+            />
+          </DynamicLink>
         </div>
         {/* Motion.div makes the div animated */}
       </motion.div>
-    </nav>
+    </header>
   );
 }
